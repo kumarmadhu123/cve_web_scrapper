c3dc20bd432067a22ccaa6d42fe9e263e2ccc5ea
Madhusudan Kumar <kumarrecover@gmail.com>
2022-01-23 22:17:45 +0530

    Seperate debian.sh with debian search in sepeate .sh script

--- main.sh
+++ main.sh
@@ -3,7 +3,7 @@
 echo "Start with web scrapping"
 
 read CVE_ID
-echo "CVE ID entered by you is $CVE_ID"
+echo "CVE ID Requested:  $CVE_ID"
 
 DIR="$PWD/store"
 echo $DIR
@@ -13,9 +13,9 @@ if [ ! -d "$DIR" ]; then
 	mkdir store
 fi
 curl "https://security-tracker.debian.org/tracker/$CVE_ID" >store/debian.txt
-#curl "https://ubuntu.com/security/$CVE_ID" >store/>buntu.txt
+#curl "https://ubuntu.com/security/$CVE_ID" >store/ubuntu.txt
 #curl "https://bugs.gentoo.org/show_bug.cgi?id=$CVE_ID" >store/gentoo.txt
 #curl "https://bugzilla.suse.com/show_bug.cgi?id=$CVE_ID" > store/bugzilla.txt
 #curl "https://bugzilla.redhat.com/show_bug.cgi?id=$CVE_ID" >store/redhat.txt 
 
-
+./debian.sh
